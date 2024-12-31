2437eb1616cfcc0940f98f9f0ed1332542b35243
khaoula <khaoulahouda140@gmail.com>
2024-12-31 05:36:37 +0100

    adding all files

---
+++ ProjetPfa/ModelView/DetailCompteVM.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace ProjetPfa.ModelView
+{
+    public class DetailCompteVM
+    {
+        [Required(ErrorMessage = "Le nom d'affichage est requis.")]
+        [Display(Name = "Nom d'affichage")]
+        public string DisplayName { get; set; }
+
+        [Required(ErrorMessage = "La description est requise.")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Photo de profil")]
+        public byte[] ProfilePicture { get; set; }
+
+        [Required(ErrorMessage = "Au moins une langue est requise.")]
+        [Display(Name = "Langues")]
+        public List<string> Languages { get; set; }
+
+        [Required(ErrorMessage = "Au moins un niveau de langue est requis.")]
+        [Display(Name = "Niveaux de langue")]
+        public List<string> LanguageLevels { get; set; }
+
+        [Display(Name = "Nom ou le numéro de votre organisation")]
+        public string OrgName { get; set; } // Added property
+    }
+}
+
+
